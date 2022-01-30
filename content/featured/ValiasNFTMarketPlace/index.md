842e2a0e04e6a7c3c2d889f48639c5bb3fa80b34
Xavier <xavierh93@hotmail.com>
2022-01-30 14:02:18 -0500

    upload images

---
+++ content/featured/ValiasNFTMarketPlace/index.md
@@ -0,0 +1,17 @@
+---
+date: '1'
+title: 'Valias NFT Marketplace'
+cover: './demo.png'
+github: 'https://github.com/CameronHixenbaugh/The-Vault-Partial'
+external: 'http://vaultv2.herokuapp.com/#/'
+tech:
+  - React
+  - Express
+  - Cadence (Smart Contract Language on Flow Blockchain)
+  - IPFS
+  - Pinata
+  - Postgres
+  - Heroku
+---
+
+A web 3.0 application built on the Flow blockchain. Valias is a NFT marketplace where you can create, buy, and sell NFTs. I am currently working on this project with two of my friends. I joined about halfway through development but have contributing consistently. React was used for the front end, express for backend, Cadence for the smart contracts, and IPFS, Pinata and Postgres for database. The site is hosted on Heroku.
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,14 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import { Layout, Hero, About, Jobs, Featured, Projects, Contact } from '@components';
+import React from "react"
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { Head } from '@components'
 
-const StyledMainContainer = styled.main`
-  counter-reset: section;
-`;
 
-const IndexPage = ({ location }) => (
-  <Layout location={location}>
-    <StyledMainContainer className="fillHeight">
-      <Hero />
-      <About />
-      <Jobs />
-      <Featured />
-      <Projects />
-      <Contact />
-    </StyledMainContainer>
-  </Layout>
-);
+// markup
+const IndexPage = () => {
+  return (
+    <h1>Hi</h1>
+  )
+}
 
-IndexPage.propTypes = {
-  location: PropTypes.object.isRequired,
-};
-
-export default IndexPage;
+export default IndexPage
